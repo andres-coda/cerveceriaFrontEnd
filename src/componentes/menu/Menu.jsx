0ea268c652772535445ddfc1f6eab2ed68c57c43
andres-coda <codaandres@gmail.com>
2023-09-06 10:15:29 -0300

    agregue menuDetalles y estilos a boton y parrafo

--- src/componentes/menu/Menu.jsx
+++ src/componentes/menu/Menu.jsx
@@ -1,9 +1,15 @@
 import './Menu.css';
 import { useContext } from 'react';
 import { contexto } from '../contexto/contexto';
+import { useNavigate } from 'react-router-dom';
 import MenuTarjeta from '../menuTarjeta/MenuTarjeta';
 function Menu() {
     const { datos } = useContext(contexto);
+    const navigate = useNavigate();
+    const btnClick = (e) => {
+        const btn = e.currentTarget.id;
+        if (btn) navigate(`/menu/${btn}`);
+    }
     return (
         <div className='menu'>
             {datos.categorias.map((dato)=> ( dato !== "todas" && dato !== "Comida Vegetariana" ? (
@@ -12,7 +18,7 @@ function Menu() {
                     <div className='menuCategoria'>
                     {datos.data.map((menu)=>{
                         if (menu.category === dato) {
-                            return <MenuTarjeta key={menu.id} dato={menu} /> 
+                            return <MenuTarjeta key={menu.id} dato={menu} click={btnClick}/> 
                         } else {
                             return null
                         }
--- src/componentes/parrafo/Parrafo.jsx
+++ src/componentes/parrafo/Parrafo.jsx
@@ -1,3 +1,4 @@
+import './Parrafo.css'
 function Parrafo({texto }) {
     return (
         <p>{texto}</p>
