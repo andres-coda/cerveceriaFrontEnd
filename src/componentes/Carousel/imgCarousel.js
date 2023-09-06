237eeb48eb75ae9e41a79c28eab0cab2217a0655
carlaarean79 <carlaandreaarean@gmail.com>
2023-09-06 10:01:42 -0300

    nuevo carousel

---
+++ src/componentes/Carousel/imgCarousel.js
@@ -0,0 +1,15 @@
+
+
+export const imgCarousel = [
+    {
+      id: 1, imgUrl: 'https://s7e6w6d2.rocketcdn.me/wp-content/uploads/2013/02/Antares--696x463.jpg',
+      caption: 'GREEN BEER',
+      caption2: 'Pasión por lo que hacemos desde 1999' 
+    },
+    {
+      id: 2, imgUrl: 'https://toledodiario.es/wp-content/uploads/2019/04/Fabrica-cervezas-Sagra_EDIIMA20190416_0721_20.jpg',
+    },
+    {
+      id: 3, imgUrl: 'https://fotos.perfil.com/2023/02/21/trim/1280/720/cerveza-artesanal-sector-en-riesgo-por-falta-de-lupulo-1513243.jpg',
+    }
+  ];
