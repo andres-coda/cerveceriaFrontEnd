5164c09cde73a6c70d638f83bbf1b206bf0961ad
FACUNDO-ALVAREZ-TORRIGLIA <facualvarezt@gmail.com>
2023-09-13 14:52:33 -0300

    Validacion de Inicio Sesion

--- src/componentes/auth/Login.jsx
+++ src/componentes/auth/Login.jsx
@@ -2,10 +2,12 @@ import { useState } from "react";
 import FormularioInput from '../formularioInput/FormularioInput';
 import Boton from "../boton/Boton";
 import "./LogReg.css";
+import Alerta from "../alerta/alerta";
 
 function Login() {
-  const [login, setLogin] = useState({})
-
+  const [login, setLogin] = useState({});
+  const [mensaje, setMensaje] = useState({});
+  const { email, password } = login;
 
   const onChan = (e) => {
     setLogin({
@@ -16,7 +18,19 @@ function Login() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-   console.log(login);
+    if ([email, password].includes("")) {
+      setMensaje({
+        msj: "Los campos deben completarse",
+        error: true
+      });
+      return;
+    }
+    // Aca puede ir validacion si existe el usuario?
+
+    setMensaje({
+      msj: "Inicio de sesión exitoso",
+      error: false
+    });
   };
 
   return (
@@ -24,16 +38,18 @@ function Login() {
       <h2>Iniciar Sesión</h2>
       <hr />
       <form onSubmit={handleSubmit} className="formulario">
-          <FormularioInput id={`email`} tipo={`email`} texto={"Correo Electrónico "} onChan={onChan} />
-          <FormularioInput id={`password`} tipo={`password`} texto={"Contraseña "} onChan={onChan} />
-          <a href="/registro">
-            <p>Registro</p>
-          </a>
+        <FormularioInput id={`email`} tipo={`email`} texto={"Correo Electrónico "} onChan={onChan} />
+        <FormularioInput id={`password`} tipo={`password`} texto={"Contraseña "} onChan={onChan} />
+        <a href="/registro">
+          <p>Registro</p>
+        </a>
         <Boton btn={{ id: "enviar", clase: "comun", texto: "Iniciar Sesión" }} btnClick={handleSubmit} />
       </form >
+      {
+        mensaje.msj && <Alerta mensaje={mensaje} />
+      }
     </div >
   );
 }
 
 export default Login;
-
