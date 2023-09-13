d469f320db2e16db20a3afa816fb75f992ca6169
FACUNDO-ALVAREZ-TORRIGLIA <facualvarezt@gmail.com>
2023-09-13 02:29:28 -0300

    Validacion de Registro

--- src/componentes/auth/Registro.jsx
+++ src/componentes/auth/Registro.jsx
@@ -2,39 +2,61 @@ import { useState } from "react";
 import "./LogReg.css";
 import FormularioInput from "../formularioInput/FormularioInput";
 import Boton from "../boton/Boton";
+import Alerta from "../alerta/alerta";
 
-function Registro(){
+function Registro() {
 
-    const [login, setLogin] = useState({})
+    const [login, setLogin] = useState({});
+    const [mensaje, setMensaje] = useState({});
+    const { email, password, repetir } = login;
 
 
     const onChan = (e) => {
-      setLogin({
-        ...login,
-        [e.target.name]: e.target.value
-      })
+        setLogin({
+            ...login,
+            [e.target.name]: e.target.value
+        })
     }
-    
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log("Te Registraste con exito");
+        if ([email, password, repetir].includes("")) {
+            setMensaje({
+                msj: "Los campos deben Completarse",
+                error: true
+            })
+            return;
+        }
+        if (password !== repetir) {
+            setMensaje({
+                msj: "Las Contraseñas deben ser iguales",
+                error: true
+            })
+            return;
+        }
+        setMensaje({
+            msj: "Rgistrado con exito",
+            error: false
+        })
     }
-
-
     return (
         <div className="conteinerGeneral login">
-                    <h2>Registro</h2>
-                    <hr />
-                    <form onSubmit={handleSubmit} className="formulario">
-                    <FormularioInput id={`email`} tipo={`email`} texto={"Correo Electrónico "} onChan={onChan} />
-                    <FormularioInput id={`password`} tipo={`password`} texto={"Contraseña "} onChan={onChan} />
-                    <FormularioInput id={`repetir`} tipo={`password`} texto={"Repetir Contraseña "} onChan={onChan} />
-                    <a href="/login">
-                        <p>Iniciar Sesion</p>
-                    </a>
-                    <Boton btn={{ id: "enviar", clase: "comun", texto: "Registrase" }} btnClick={handleSubmit} />
-                    </form>
-                </div>
+            <h2>Registro</h2>
+            <hr />
+            <form onSubmit={handleSubmit} className="formulario">
+                <FormularioInput id={`email`} tipo={`email`} texto={"Correo Electrónico "} onChan={onChan} />
+                <FormularioInput id={`password`} tipo={`password`} texto={"Contraseña "} onChan={onChan} />
+                <FormularioInput id={`repetir`} tipo={`password`} texto={"Repetir Contraseña "} onChan={onChan} />
+                <a href="/login">
+                    <p>Iniciar Sesion</p>
+                </a>
+                <Boton btn={{ id: "enviar", clase: "comun", texto: "Registrase" }} btnClick={handleSubmit} />
+            </form>
+            {
+                mensaje.msj && <Alerta mensaje={mensaje} />
+            }
+
+        </div>
     )
 }
 
