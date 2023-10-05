ca4fa1476405976f1df0c08c77c5751292b4a004
andres-coda <codaandres@gmail.com>
2023-10-05 19:21:18 -0300

    modal carrito terminado

--- src/componentes/reservas/ModalReservas.jsx
+++ src/componentes/reservas/ModalReservas.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './ModalReservas.css'
 import Boton from '../boton/Boton';
 
-function ModalReservas({ isVisible, onClose, reserva, claveReserva }) {
+function ModalReservas({ isVisible, onClose, reserva, claveReserva, estado, titulo }) {
   if (!isVisible) return null;
 
   return (
@@ -10,11 +10,11 @@ function ModalReservas({ isVisible, onClose, reserva, claveReserva }) {
 
       <img src="src/assets/Logo.png" alt="Logo del Restaurante" />
       <span className='line-modal'></span>
-      <h3>¡¡Reserva Exitosa!!</h3>
+      <h3>¡¡{titulo}!!</h3>
       <p>Nombre y Apellido: <strong> {reserva.nombre} {reserva.apellido} </strong> </p>
       <p>Fecha: <strong>{reserva.fecha}</strong></p>
       <p>Hora: <strong>{reserva.hora}</strong></p>
-      <p>Cantidad de Personas: <strong>{reserva.personas}</strong></p>
+      <p>{estado} <strong>{reserva.personas}</strong></p>
       <p>Clave de Reserva: <strong>{claveReserva}</strong></p>
       <span className='line-modal'></span>
       <p>El Ticket de Reserva se ha enviado al email: <strong>{reserva.email}</strong></p>
