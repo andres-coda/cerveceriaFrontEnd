21e5f01f0bdb3302e9222359f9f7be536cfcd860
carlaarean79 <carlaandreaarean@gmail.com>
2023-10-12 00:14:56 -0300

    detalles finales

--- README.txt
+++ README.txt
@@ -1,3 +1,16 @@
+ Participantes del grupo
+ Arean, Carla      https://github.com/carlaarean79
+                   https://www.linkedin.com/in/carla-arean-7692b3270/
+
+ Coda, Andrés      https://github.com/andres-coda
+                   https://www.linkedin.com/in/andr%C3%A9s-coda-83824027a/
+
+ Alvarez, Facundo  https://github.com/Facundo-Alvarez-Torriglia
+                   https://www.linkedin.com/in/facundo-alvarez-7b23a0236/
+
+ Curuchet, Juan    https://github.com/juanjocurutchet
+                   https://www.linkedin.com/in/juanjo-curutchet78/
+-------------------------------------------------------------------------------------                 
  Enlace al tablero de Trello
 
  https://trello.com/b/8pDU84Pb/comienzo-del-frontend
@@ -6,15 +19,22 @@
 
  https://docs.google.com/document/d/1UNWzZe-vZ0EyiXH4NtwVjV2SZll119gQ80QSTMH4H-A/edit
  
- 
- Depencias instaladas
- google map api
- 
-npm i -S @react-google-maps/api
+ Link al proyecto en GIt hub
+ https://github.com/andres-coda/cerveceriaFrontEnd.git
+------------------------------------------------------------------- 
+comandos para correr el programa en React
+npm run dev
+-------------------------------------------------------------------------
+Se deberá tener en cuenta de correr en simultaneo el programa que contiene la api con el CRUD
+comandos para correr las dos terminales
+npm run db
+npm run start:dev
 
-
-
+Link al proyecto en Git hub
+https://github.com/andres-coda/cerveceriaBackEnd.git
 ------------------------------------------------------------------
+Instalación del recurso Mapa de leaflet
+-------------------------------------------------------------------
 npm install react-leaflet leaflet
 
 comandos de actualización 
@@ -37,14 +57,14 @@ npm install @babel/plugin-transform-optional-chaining
 creación de rama nueva git hub
 git checkout -b ----  //creación de rama nueva
 
-git push --set-upstream origin Carla // primer comando para push rama nueva
+git push --set-upstream origin Pepe // primer comando para push rama nueva
 
 
 // luego git push solamente
 
-
+-----------------------------------------------------------------------
 //paleta de colores
-
+-----------------------------------------------------------------------
 Dominante: #1D1D1B
 Subordinado: #F1F1EF
 Detalles: #F1D69d - #c19f71 - #fbf8e9
--- src/componentes/Contacto/Contacto.jsx
+++ src/componentes/Contacto/Contacto.jsx
@@ -2,36 +2,65 @@ import React, { useState } from 'react';
 import './Contacto.css';
 import Subtitulo from '../subtitulo/Subtitulo';
 import Parrafo from '../parrafo/Parrafo';
-import FormularioInput from '../formularioInput/FormularioInput'
 import Boton from '../boton/Boton';
 import {IoBeerOutline} from 'react-icons/io5';
 import { useNavigate } from "react-router-dom";
 
-
 function Contacto() {
-  const navegate = useNavigate()
+  const navigate = useNavigate();
   const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
     email: '',
     message: '',
   });
 
   const [mensajeEnviado, setMensajeEnviado] = useState(false);
+  const [errors, setErrors] = useState({});
 
-  const onChan = (e) => {
-    e.preventDefault();
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-    console.log(e.target.name);
-    console.log(e.target.value);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
   };
-  
+
+  const validateForm = () => {
+    const errors = {};
+
+    if (!formData.firstName) {
+      errors.firstName = 'Por favor, ingrese su nombre';
+    }
+
+    if (!formData.lastName) {
+      errors.lastName = 'Por favor, ingrese su apellido';
+    }
+
+    if (!formData.email) {
+      errors.email = 'Por favor, ingrese su correo electrónico';
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      errors.email = 'El correo electrónico no es válido';
+    }
+
+    if (!formData.message) {
+      errors.message = 'Por favor, ingrese su mensaje';
+    }
+
+    setErrors(errors);
+
+    return Object.keys(errors).length === 0;
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    setMensajeEnviado(true);
-    console.log(formData);
+    
+    if (validateForm()) {
+      setMensajeEnviado(true);
+      console.log(formData);
+    }
   };
+
   const mensaje = (e) => {
     e.preventDefault();
-    navegate("/");
+    navigate("/");
   }
 
   return (
@@ -42,33 +71,56 @@ function Contacto() {
         </div>
         <div className='container-form'>
           {mensajeEnviado ? (
-              <div className='mensaje-Enviado'>
+            <div className='mensaje-Enviado'>
               <p>Su mensaje ha sido enviado exitosamente.</p>
               <p>En breve responderemos a su consulta.</p>
               <p>Muchas gracias por elegirnos! Grupo Green Beer.<IoBeerOutline className='beer-icon' /></p>
               <Boton btn={{ id: "Salir", clase: "comun", texto: "Salir" }} btnClick={mensaje} />
-
             </div>
           ) : (
             <>
-            <Parrafo clase={"form-p"} texto={'¡Queremos conocerte. Dejanos tu mensaje y responderemos a la brevedad!'} />
-                <input id='email' type='email' onChange={onChan} placeholder='Email' />
+              <Parrafo clase={"form-p"} texto={'¡Queremos conocerte. Dejanos tu mensaje y responderemos a la brevedad!'} />
+              <form onSubmit={handleSubmit}>
+                <input
+                  id='consulta'
+                  type="text"
+                  name="firstName"
+                  value={formData.firstName}
+                  onChange={handleChange}
+                  placeholder="Nombre"
+                  required />
+                {errors.firstName && <p className="error">{errors.firstName}</p>}
+                <input
+                  type="text"
+                  name="lastName"
+                  value={formData.lastName}
+                  onChange={handleChange}
+                  placeholder="Apellido"
+                  required />
+                {errors.lastName && <p className="error">{errors.lastName}</p>}
+                <input
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  placeholder="Correo electrónico"
+                  required />
+                {errors.email && <p className="error">{errors.email}</p>}
                 <textarea
-                 className='textarea'
-                name="message"
-                value={formData.message}
-                onChange={onChan} 
-                placeholder="Deje aquí su comentario"
-                required
-                />
-              <Boton btn={{ id: "enviar", clase: "comun", texto: "Enviar" }} btnClick={handleSubmit} />
-                </>
-          
+                  className='textarea'
+                  name="message"
+                  value={formData.message}
+                  onChange={handleChange}
+                  placeholder="Mensaje"
+                  required />
+                {errors.message && <p className="error">{errors.message}</p>}
+                <Boton btn={{ id: "enviar", clase: "comun", texto: "Enviar" }} btnClick={handleSubmit}/>
+              </form>
+            </>
           )}
         </div>
-        <div className="imgLupuloContacto"> {/*componente fotoQSomos no funciona con la img*/}
-          
-            <img src="https://rentabilibar.s3-eu-west-1.amazonaws.com/media/lupulo_planta.jpg" alt="Lúpulo" />
+        <div className="imgLupuloContacto">
+          <img src="https://rentabilibar.s3-eu-west-1.amazonaws.com/media/lupulo_planta.jpg" alt="Lúpulo" />
         </div>
       </div>
     </section>
