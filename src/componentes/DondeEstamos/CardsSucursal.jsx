1bca5bb243c6d50669b6afc18c34064f2e2a749b
carlaarean79 <carlaandreaarean@gmail.com>
2024-05-26 16:05:54 -0300

    sursales para administrador

--- src/componentes/DondeEstamos/CardsSucursal.jsx
+++ src/componentes/DondeEstamos/CardsSucursal.jsx
@@ -1,8 +1,8 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext, useState } from 'react';
 import { FaFacebook, FaWhatsapp, FaInstagram, FaMailBulk, FaEdit, FaTrash } from 'react-icons/fa';
 import { CiLocationOn } from 'react-icons/ci';
 import { contexto } from '../contexto/contexto';
-import { fetchDelete, fetchPut } from '../funciones fetch/funciones';
+import { fetchDelete, fetchPatCh, fetchPut } from '../funciones fetch/funciones';
 import { URL_SUCURSAL } from '../../endPoints/endPoints';
 import { VscLayersActive } from "react-icons/vsc";
 import Boton from '../boton/Boton';
@@ -11,71 +11,104 @@ import { useNavigate } from 'react-router-dom';
 const CardsSucursal = ({ sucursal }) => {
     const { setDatos, datos } = useContext(contexto);
     const [isEditing, setIsEditing] = useState(false);
-    const navegate = useNavigate();
+    const navigate = useNavigate();
 
     const [editedSucursal, setEditedSucursal] = useState({
-        nombre: sucursal.nombre || '',
-        direccion: sucursal.direccion || '',
-        telefono: sucursal.telefono || '',
-        email: sucursal.email || '',
-        instagram: sucursal.instagram || '',
-        facebook: sucursal.facebook || ''
-      });
-   
+        imagen: "",
+        nombre: "",
+        direccion: "",
+        telefono: "",
+        email: "",
+        instagram: "",
+        facebook: ""
+    });
 
-      useEffect(() => {
-        // Llamada a la API para obtener los datos de la sucursal
-        // Luego, establece el estado con los valores obtenidos
+    const startEditing = () => {
         setEditedSucursal({
-          nombre: sucursal.nombre || '',
-          direccion: sucursal.direccion || '',
-          telefono: sucursal.telefono || '',
-          email: sucursal.email || '',
-          instagram: sucursal.instagram || '',
-          facebook: sucursal.facebook || ''
+            imagen:sucursal.imagen,
+            nombre: sucursal.nombre,
+            direccion: sucursal.direccion,
+            telefono: sucursal.telefono,
+            email: sucursal.email,
+            instagram: sucursal.instagram,
+            facebook: sucursal.facebook
         });
-      }, [sucursal]);
-      
-
+        setIsEditing(true);
+    };
 
     const handleEditChange = (e) => {
-        const { name, value } = e.target;
-        setEditedSucursal({ ...editedSucursal, [name]: value });
+        setEditedSucursal({ ...editedSucursal, [e.target.name]: e.target.value });
     };
 
     const handleSave = async (e) => {
         e.preventDefault();
         try {
-            const response = await fetchPut(`${URL_SUCURSAL}/${editedSucursal.id}`, localStorage.getItem('token'), editedSucursal);
-            console.log(editedSucursal ,'put entrada');
-
+            const response = await fetchPut(`${URL_SUCURSAL}/${sucursal.id}`, localStorage.getItem('token'), editedSucursal);
             if (response) {
                 setDatos((prev) => ({ ...prev, refresh: true }));
-                setIsEditing(false); // Finalizar el modo de edición
-                          }
-          
-
+                setIsEditing(false);
+                navigate('/dondeestamos')
+            }
         } catch (error) {
             console.error("Error al editar la sucursal:", error);
         }
     };
 
-    const handleDelete = async () => {
-        try {
-            const response = await fetchDelete(`${URL_SUCURSAL}/${sucursal.id}`, localStorage.getItem('token'));
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
+    const DeleteSucursal = async () => {
+        // Mostrar el diálogo de confirmación antes de eliminar
+        const isConfirmed = window.confirm("¿Estás seguro de que deseas eliminar esta sucursal?");
+        
+        // Si el usuario confirma, proceder con la eliminación
+        if (isConfirmed) {
+            try {
+                const response = await fetchDelete(`${URL_SUCURSAL}/${sucursal.id}`, localStorage.getItem('token'));
+                
+                if (response.ok) {
+                    // Mostrar mensaje de eliminación exitosa
+                    alert(`Sucursal ${sucursal.nombre} eliminada correctamente`);
+                } else {
+                    // Manejar la respuesta cuando no se elimina correctamente
+                    setDatos((prev) => ({ ...prev, refreshSucursal: true }));
+                }
+            } catch (error) {
+                console.error('Error al eliminar sucursal:', error);
             }
-            setDatos((prev) => ({ ...prev, refreshSucursal: true }));
-        } catch (error) {
-            console.error('Error al eliminar sucursal:', error);
+        } else {
+            // Si el usuario cancela, no se hace nada
+            console.log("Eliminación cancelada por el usuario");
         }
     };
+
+    const reactivarSucursal = async () => {
+        // Mostrar el diálogo de confirmación antes de eliminar
+        const isConfirmed = window.confirm("¿Estás seguro de que deseas restaurar la eliminación de esta sucursal?");
+        
+        // Si el usuario confirma, proceder con la eliminación
+        if (isConfirmed) {
+            try {
+                const response = await fetchPatCh(`${URL_SUCURSAL}/${sucursal.id}`, localStorage.getItem('token'));
+                
+                if (response.ok) {
+                    alert(`Sucursal ${sucursal.nombre} restaurada correctamente`);
+                } else {
+                    setDatos((prev) => ({ ...prev, refreshSucursal: true }));
+                }
+            } catch (error) {
+                alert('Error al restaurar la sucursal:', error);
+            }
+        } else {
+            alert("Restauración cancelada por el usuario");
+        }
+    };
+    
 
     return (
         <div className={sucursal.deleted ? 'cards-sucursalEliminada' : 'cards-sucursal'}>
             {isEditing ? (
                 <div>
+                    <div className='card-img-container'>
+                    <input name="imagen"  src={editedSucursal.imagen} alt={sucursal.nombre} />
+                    </div>
                     <input name="nombre" value={editedSucursal.nombre} onChange={handleEditChange} />
                     <input name="direccion" value={editedSucursal.direccion} onChange={handleEditChange} />
                     <input name="telefono" value={editedSucursal.telefono} onChange={handleEditChange} />
@@ -100,13 +133,13 @@ const CardsSucursal = ({ sucursal }) => {
             <div className='btn-icon'>
                 {datos.userAct && datos.userAct.role === "admin" ? (
                     <div>
-                        <button onClick={() => setIsEditing(true)} title='Editar Surcursal'><FaEdit /></button>
-                        <button onClick={handleDelete} title='Eliminar Surcursal'><FaTrash /></button>
+                        <button onClick={startEditing} title='Editar Surcursal'><FaEdit /></button>
+                        <button onClick={DeleteSucursal} title='Eliminar Surcursal'><FaTrash /></button>
                     </div>
                 ) : (null)}
                 {sucursal && sucursal.deleted ? (
                     <div>
-                        <button onClick={() => setIsEditing(true)} title='Restaurar Surcursal Eliminada'><VscLayersActive /></button>
+                        <button onClick={reactivarSucursal} title='Reactivar Surcursal Eliminada'><VscLayersActive /></button>
                     </div>
                 ) : (null)}
             </div>
--- src/componentes/contexto/contexto.jsx
+++ src/componentes/contexto/contexto.jsx
@@ -18,7 +18,7 @@ export const ProveedorContexto = ({children}) => {
         usuario:[], 
         usuarioActivo: {
             usuario:{user: "login"}, 
-            administrador: fal
+            administrador: false
         }, 
         datoAEditar: null, 
         productos:[], 
