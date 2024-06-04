302962d851350cffd3f93ef712a2384f67ef9bc2
carlaarean79 <carlaandreaarean@gmail.com>
2024-06-04 17:40:45 -0300

    ayuda con donde estamos!

--- src/componentes/DondeEstamos/CardsSucursal.jsx
+++ src/componentes/DondeEstamos/CardsSucursal.jsx
@@ -4,14 +4,19 @@ import { CiLocationOn } from 'react-icons/ci';
 import { contexto } from '../contexto/contexto';
 import { fetchDelete, fetchPatCh, fetchPut } from '../funciones fetch/funciones';
 import { URL_SUCURSAL } from '../../endPoints/endPoints';
-import { VscLayersActive } from "react-icons/vsc";
 import Boton from '../boton/Boton';
 import { useNavigate } from 'react-router-dom';
 import FormularioInput from '../formularioInput/FormularioInput';
+import Modal from '../modal/modal';
+import AlertSucursal from '../alertSucursal/AlertSucursal';
+import AnimatedSVG from '../animacion/AnimatedSVG';
+import './CardsSucursal.css';  // Archivo CSS para los estilos
 
 const CardsSucursal = ({ sucursal }) => {
+    const [alerta, setAlerta] = useState({ estado: false, tipo: '', idTexto: '', mensaje: '' });
     const { setDatos, datos } = useContext(contexto);
     const [isEditing, setIsEditing] = useState(false);
+    const [isSaving, setIsSaving] = useState(false);
     const navigate = useNavigate();
 
     const [editedSucursal, setEditedSucursal] = useState({
@@ -26,7 +31,7 @@ const CardsSucursal = ({ sucursal }) => {
 
     const startEditing = () => {
         setEditedSucursal({
-            imagen:sucursal.imagen,
+            imagen: sucursal.imagen,
             nombre: sucursal.nombre,
             direccion: sucursal.direccion,
             telefono: sucursal.telefono,
@@ -34,7 +39,7 @@ const CardsSucursal = ({ sucursal }) => {
             instagram: sucursal.instagram,
             facebook: sucursal.facebook
         });
-        setIsEditing(true);
+        setAlerta({ estado: true, tipo: 'editar', idTexto: 'editar', mensaje: '¿Seguro que quiere editar esta sucursal?' });
     };
 
     const handleEditChange = (e) => {
@@ -43,119 +48,136 @@ const CardsSucursal = ({ sucursal }) => {
 
     const handleSave = async (e) => {
         e.preventDefault();
+        setIsSaving(true);
         try {
             const response = await fetchPut(`${URL_SUCURSAL}/${sucursal.id}`, localStorage.getItem('token'), editedSucursal);
             if (response) {
                 setDatos((prev) => ({ ...prev, refresh: true }));
                 setIsEditing(false);
-                navigate('/dondeestamos')
+                navigate('/dondeestamos');
             }
         } catch (error) {
             console.error("Error al editar la sucursal:", error);
+        } finally {
+            setIsSaving(false);
         }
     };
 
-    const DeleteSucursal = async () => {
-        // Mostrar el diálogo de confirmación antes de eliminar
-        const isConfirmed = window.confirm("¿Estás seguro de que deseas eliminar esta sucursal?");
-        
-        // Si el usuario confirma, proceder con la eliminación
-        if (isConfirmed) {
-            try {
-                const response = await fetchDelete(`${URL_SUCURSAL}/${sucursal.id}`, localStorage.getItem('token'));
-                
-                if (response.ok) {
-                    // Mostrar mensaje de eliminación exitosa
-                    alert(`Sucursal ${sucursal.nombre} eliminada correctamente`);
-                } else {
-                    // Manejar la respuesta cuando no se elimina correctamente
-                    setDatos((prev) => ({ ...prev, refreshSucursal: true }));
-                }
-            } catch (error) {
-                console.error('Error al eliminar sucursal:', error);
-            }
-        } else {
-            // Si el usuario cancela, no se hace nada
-            console.log("Eliminación cancelada por el usuario");
-        }
+    const DeleteSucursal = () => {
+        setAlerta({ estado: true, tipo: 'eliminar', idTexto: 'eliminar', mensaje: '¿Seguro que quiere eliminar esta sucursal?' });
     };
 
-    const reactivarSucursal = async () => {
-        // Mostrar el diálogo de confirmación antes de eliminar
-        const isConfirmed = window.confirm("¿Estás seguro de que deseas restaurar la eliminación de esta sucursal?");
-        
-        // Si el usuario confirma, proceder con la eliminación
-        if (isConfirmed) {
-            try {
-                const response = await fetchPatCh(`${URL_SUCURSAL}/${sucursal.id}`, localStorage.getItem('token'));
-                
-                if (response.ok) {
-                    alert(`Sucursal ${sucursal.nombre} restaurada correctamente`);
-                } else {
-                    setDatos((prev) => ({ ...prev, refreshSucursal: true }));
+    const reactivarSucursal = () => {
+        setAlerta({ estado: true, tipo: 'reactivar', idTexto: 'reactivar', mensaje: '¿Seguro que quiere restaurar la eliminación de esta sucursal?' });
+    };
+
+    const handleAlertAction = async (action) => {
+        switch (action) {
+            case 'editar':
+                setIsEditing(true);
+                setAlerta({ estado: false, tipo: '', idTexto: '' });
+                break;
+            case 'eliminar':
+                try {
+                    const response = await fetchDelete(`${URL_SUCURSAL}/${sucursal.id}`, localStorage.getItem('token'));
+                    if (response) {
+                        setDatos((prev) => ({ ...prev, refreshSucursal: true }));
+                    }
+                } catch (error) {
+                    console.error('Error al eliminar sucursal:', error);
                 }
-            } catch (error) {
-                alert('Error al restaurar la sucursal:', error);
-            }
-        } else {
-            alert("Restauración cancelada por el usuario");
+                setAlerta({ estado: false, tipo: '', idTexto: '' });
+                break;
+            case 'reactivar':
+                try {
+                    const response = await fetchPatCh(`${URL_SUCURSAL}/${sucursal.id}`, localStorage.getItem('token'));
+                    if (response) {
+                        setDatos((prev) => ({ ...prev, refreshSucursal: true }));
+                    }
+                } catch (error) {
+                    alert('Error al restaurar la sucursal:', error);
+                }
+                setAlerta({ estado: false, tipo: '', idTexto: '' });
+                break;
+            case 'cancelar':
+            default:
+                setAlerta({ estado: false, tipo: '', idTexto: '' });
+                break;
         }
     };
-    
 
     return (
-        <div className={sucursal.deleted ? 'cards-sucursalEliminada' : 'cards-sucursal'}>
-            {isEditing ? (
-                <div>
-                    <div className='card-img-container'>
-                    <input name="imagen"  value={editedSucursal.imagen} onChange={handleEditChange} />
-                    </div>
-                    <FormularioInput id={"nombre"} value={sucursal.nombre} tipo={"text"} texto={"Nombre"} onChan={handleEditChange} />
-                    <FormularioInput id={"direccion"} value={sucursal.direccion} tipo={"text"} texto={"Dirección"} onChan={handleEditChange} />
-                    <FormularioInput id={"telefono"} value={sucursal.telefono} tipo={"text"} texto={"Teléfono"} onChan={handleEditChange} />
-                    <FormularioInput id={"email"} value={sucursal.email} tipo={"text"} texto={"Email"} onChan={handleEditChange} />
-                    <FormularioInput id={"instagram"} value={sucursal.instagram} tipo={"text"} texto={"Instagram"} onChan={handleEditChange} />
-                    <FormularioInput id={"facebook"} value={sucursal.facebook} tipo={"text"} texto={"Facebook"} onChan={handleEditChange} />
-                    <FormularioInput id={"imagen"} value={sucursal.imagen} tipo={"text"} texto={"Url imagen"} onChan={handleEditChange} />
-            
-                    <input name="nombre" value={editedSucursal.nombre} onChange={handleEditChange} />
-                    <input name="direccion" value={editedSucursal.direccion} onChange={handleEditChange} />
-                    <input name="telefono" value={editedSucursal.telefono} onChange={handleEditChange} />
-                    <input name="email" value={editedSucursal.email} onChange={handleEditChange} />
-                    <input name="instagram" value={editedSucursal.instagram} onChange={handleEditChange} />
-                    <input name="facebook" value={editedSucursal.facebook} onChange={handleEditChange} />
-                    <Boton btn={{ id: "editSucursal", clase: "comun", texto: "Guardar" }} btnClick={handleSave} />
-                </div>
-            ) : (
-                <div>
-                    <div className='card-img-container'>
+        //cards-sucursal contenedor total
+        <div className={`cards-sucursal ${sucursal.deleted ? 'cards-sucursalEliminada' : ''}`}>
+            {isEditing && (
+                //modal vista admin para adminsitrar la sucursal
+                <Modal isOpen={isEditing} onClose={() => setIsEditing(false)}>
+                    {isSaving ? (
+                        <AnimatedSVG />
+                    ) : (
+                        //card-img-form clase de la imagen
+                        <>
+                        
+                            <img className='card-img-form' src={editedSucursal.imagen} alt={sucursal.nombre} />
+                            <form onSubmit={handleSave} className='form-modal'>
+                                <FormularioInput className={'imagen'} id={"imagen"} value={editedSucursal.imagen} tipo={"text"} texto={"Url_Imagen"} onChan={handleEditChange} />
+                                <FormularioInput id={"nombre"} value={editedSucursal.nombre} tipo={"text"} texto={"Nombre"} onChan={handleEditChange} />
+                                <FormularioInput id={"direccion"} value={editedSucursal.direccion} tipo={"text"} texto={"Dirección"} onChan={handleEditChange} />
+                                <FormularioInput id={"telefono"} value={editedSucursal.telefono} tipo={"text"} texto={"Teléfono"} onChan={handleEditChange} />
+                                <FormularioInput id={"email"} value={editedSucursal.email} tipo={"text"} texto={"Email"} onChan={handleEditChange} />
+                                <FormularioInput id={"instagram"} value={editedSucursal.instagram} tipo={"text"} texto={"Instagram"} onChan={handleEditChange} />
+                                <FormularioInput id={"facebook"} value={editedSucursal.facebook} tipo={"text"} texto={"Facebook"} onChan={handleEditChange} />
+                                <div>
+                                    <Boton btn={{ id: "editar", clase: "comun", texto: "Guardar" }} btnClick={handleSave} />
+                                </div>
+                            </form>
+                        </>
+                    )}
+                </Modal>
+            )}
+            {!isEditing && (
+                //card-container contenedor vista de usuario y admin
+                <div className='card-sucursal-container'>
+                    <div className='card-img-sucursal-container'>
                         <img className='card-img' src={sucursal.imagen} alt={sucursal.nombre} />
+                            {/*card.overlay contenedor cuando hace hover vista usuario y admin*/}
+                        <div className='card-sucursal-overlay'>
+                            <h1 className='card-h1'>{sucursal.nombre}</h1>
+                            <h3 className='card-h3'><CiLocationOn className='icon' />{sucursal.direccion}</h3>
+                            <h3 className='card-h3'><FaWhatsapp className='icon' />{sucursal.telefono}</h3>
+                            <h3 className='card-h3'><FaMailBulk className='icon' />{sucursal.email}</h3>
+                            <h3 className='card-h3'><FaInstagram className='icon' />{sucursal.instagram}</h3>
+                            <h3 className='card-h3'><FaFacebook className='icon' />{sucursal.facebook}</h3>
+                        </div>
                     </div>
-                    <h1 className='card-h1'>{sucursal.nombre}</h1>
-                    <h3 className='card-h3'><CiLocationOn className='icon' />{sucursal.direccion}</h3>
-                    <h3 className='card-h3'><FaWhatsapp className='icon' />{sucursal.telefono}</h3>
-                    <h3 className='card-h3'><FaMailBulk className='icon' />{sucursal.email}</h3>
-                    <h3 className='card-h3'><FaInstagram className='icon' />{sucursal.instagram}</h3>
-                    <h3 className='card-h3'><FaFacebook className='icon' />{sucursal.facebook}</h3>
                 </div>
             )}
+            {/*btn-icon contenedor botones iconos administración */}
             <div className='btn-icon'>
-                {datos.userAct && datos.userAct.role === "admin" ? (
-                    <div>
-                        <button onClick={startEditing} title='Editar Surcursal'><FaEdit /></button>
-                        <button onClick={DeleteSucursal} title='Eliminar Surcursal'><FaTrash /></button>
-                        <button onClick={DeleteSucursal} title='Eliminar Surcursal'><FaUndo /></button>
-               
-                    </div>
-                ) : (null)}
                 {sucursal && sucursal.deleted ? (
                     <div>
-                        <button onClick={reactivarSucursal} title='Reactivar Surcursal Eliminada'><VscLayersActive /></button>
+                        <button onClick={reactivarSucursal} title='Reactivar Surcursal Eliminada' className='icon-button'><FaUndo /></button>
                     </div>
-                ) : (null)}
+                ) : (
+                    datos.userAct && datos.userAct.role === "admin" && (
+                        <div>
+                            <button onClick={startEditing} title='Editar Surcursal' className='icon-button'><FaEdit /></button>
+                            <button onClick={DeleteSucursal} title='Eliminar Surcursal' className='icon-button'><FaTrash /></button>
+                        </div>
+                    )
+                )}
             </div>
+            {alerta.estado && (
+                <AlertSucursal
+                    setAlerta={setAlerta}
+                    idTexto={alerta.idTexto}
+                    handleAlertAction={handleAlertAction}
+                    mensaje={alerta.mensaje}
+                />
+            )}
         </div>
     );
 };
 
 export default CardsSucursal;
+
--- src/componentes/DondeEstamos/DondeEstamos.jsx
+++ src/componentes/DondeEstamos/DondeEstamos.jsx
@@ -18,21 +18,11 @@ const DondeEstamos = () => {
 
     return (
         <div className='prueba' >
-            <img src='https://www.lammsbraeu.de/hs-fs/hubfs/mail_images/2023/2023%20-%2008%20-%20B2C%20Bier/Brauereif%C3%BChrung%20nach%20der%20Zwicklprobe.png?width=768&height=396&name=Brauereif%C3%BChrung%20nach%20der%20Zwicklprobe.png'/>
-{/*           <Parallax
-  bgImage='https://www.lammsbraeu.de/hs-fs/hubfs/mail_images/2023/2023%20-%2008%20-%20B2C%20Bier/Brauereif%C3%BChrung%20nach%20der%20Zwicklprobe.png?width=768&height=396&name=Brauereif%C3%BChrung%20nach%20der%20Zwicklprobe.png'
-  strength={100}
-  bgImageStyle={{
-    width: '100%', // Asegúrate de que el ancho sea el 100% del contenedor
-    height: 'auto', // Ajusta la altura automáticamente para mantener la proporción
-    backgroundSize: 'cover', // Ajusta el tamaño de la imagen para cubrir completamente el contenedor
-    backgroundPosition: 'center' // Centra la imagen en el contenedor
-  }}
->
-  <div style={{ height: '90vh', width: '100%' }}></div>
-</Parallax>
-
- */}        </div>
+       
+                        {datos.sucursales.map(sucursal => (
+                            <CardsSucursal key={sucursal.id} sucursal={sucursal} />
+                        ))}
+      </div>
     );
 };
 
